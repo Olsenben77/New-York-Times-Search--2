1d6a49eca7f9371087071de6a82f0cba3dc64ba7
Richard Wang <richardchacha@hotmail.com>
2020-01-04 13:38:57 -0800

    fix script

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 
 // API consts
 const myKey = 'qSHvM1jVAPSpzcahkXf4we3GRfxLAhRQ';
-const newyorkURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=new+york+times';
+const newyorkURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=';
+const beginYear = '&begin_date='; //string
+const endYear = '&end_date='
 
-let NYObj = {};
+// event listeners
+$('#number-records').on('click', function(){
+    console.log($(this));
+});
+
+
+// let NYObj = {};
 
 
 // relevan t JSON data
-let abstract = respose.doc[i].abstract;
-let author = respose.doc[i].byline.original;
-let section = response.doc[i].section_name;
-let publishDate = response.doc[i].pub_date;
-let articleURL = response.doc[i].web_url;
+let abstract = NYObj.response.docs[0].abstract;
+let author = NYObj.response.docs[0].byline.original;
+let section = NYObj.response.docs[0].section_name;
+let publishDate = NYObj.response.docs[0].pub_date;
+let articleURL = NYObj.response.docs[0].web_url;
 
+console.log(abstract);
